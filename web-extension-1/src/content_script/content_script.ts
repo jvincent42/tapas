c5f3b73e59564f0563da7230b402f9e79f1716b8
Jeremy Vincent <j.vincent.off@gmail.com>
2020-05-06 11:29:49 +0200

    [web-extension-1] feat(content_script): add content_script

--- web-extension-1/src/content_script/content_script.ts
+++ web-extension-1/src/content_script/content_script.ts
@@ -1,6 +1,12 @@
+interface Message {
+  counter: number;
+}
+
 const port = browser.runtime.connect(browser.runtime.id, { name: 'content_script/lifecycle' });
 
-// perform cleanup here
-port.onDisconnect.addListener(() => {
-  console.log('cleanup');
+port.onMessage.addListener((req: Message) => {
+  document.write(
+    `<div>Popup opened ${req.counter} time${req.counter === 0 || req.counter > 1 ? 's' : ''} on this tab</div>`,
+  );
+  document.close();
 });
